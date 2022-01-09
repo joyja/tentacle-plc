a3615803a792939e72060afff6459a8bca170d48
James Joy <joyja@jarautomation.io>
2022-01-09 01:13:19 +0000

    added denormalize, setValues and getValues

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 const fs = require('fs')
 const path = require('path')
 const Persistence = require('./persistence')
+const _ = require('lodash')
+const denormalize = require('./denormalize')
 
 const config = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, 'runtime/config.json'))
@@ -16,18 +18,42 @@ const app = express()
 app.use(cors())
 
 const schema = buildSchema(`
+  type atomicVariable {
+    path: String!
+    value: String!
+    datatype: String!
+  }
+  type configTask {
+    name: String!
+    description: String!
+    scanRate: Int!
+    program: String!
+  }
+  type configMqtt {
+    name: String!
+    description: String!
+    endpoint: String!
+  }
+  type config {
+    tasks: [configTask!]!
+    mqtt: [configMqtt!]!
+  }
   type taskMetric {
-    task: String!,
-    functionExecutionTime: Float!,
-    intervalExecutionTime: Float!,
+    task: String!
+    functionExecutionTime: Float!
+    intervalExecutionTime: Float!
     totalScanTime: Float!
   }
   type Mutation {
     loadProgram(program: String!): String
+    setValue(variablePath: String!, value: String!): atomicVariable
   }
   type Query {
     info: String!
     metrics: [taskMetric!]!
+    getValue(variablePath: String!): atomicVariable
+    getValues: [atomicVariable!]!
+    configuration: config!
   }
 `)
 
@@ -39,13 +65,72 @@ let persistence
 
 const rootValue = {
   info: 'A Modern SoftPLC.',
-  metrics: function (args, context, somethingelse) {
+  metrics: function (args, context, info) {
     return Object.keys(context.metrics).map((key) => {
       return {
         task: key,
         ...context.metrics[key],
       }
     })
+  },
+  configuration: function (args, context) {
+    return {
+      tasks: Object.keys(context.config.tasks).map((key) => {
+        return {
+          name: key,
+          description: context.config.tasks[key].description
+            ? context.config.tasks[key].description
+            : '',
+          ...context.config.tasks[key],
+        }
+      }),
+      mqtt: Object.keys(context.config.mqtt).map((key) => {
+        return {
+          name: key,
+          description: context.config.mqtt[key].description
+            ? context.config.mqtt[key].description
+            : '',
+          ...context.config.mqtt[key],
+        }
+      }),
+    }
+  },
+  getValue: function (args, context, info) {
+    const variable = _.get(context.global, args.variablePath)
+    if (variable !== undefined) {
+      return {
+        path: args.variablePath,
+        value: variable,
+        datatype: typeof variable,
+      }
+    } else {
+      throw Error(`${args.variablePath} does not exits.`)
+    }
+  },
+  getValues: function (args, context, info) {
+    const values = denormalize(context.global)
+    return Object.keys(values).map((key) => {
+      return {
+        path: key,
+        value: values[key],
+        datatype: typeof values[key],
+      }
+    })
+  },
+  setValue: function (args, context, info) {
+    const variable = _.get(context.global, args.variablePath)
+    if (variable !== undefined) {
+      if (typeof variable == 'boolean') {
+        _.set(context.global, args.variablePath, args.value === 'true')
+      }
+      return {
+        path: args.variablePath,
+        value: args.value,
+        datatype: typeof variable,
+      }
+    } else {
+      throw Error(`${args.variablePath} does not exits.`)
+    }
   },
   // loadProgram: (args) => {
   //   if (mainInterval) {
@@ -59,6 +144,7 @@ const rootValue = {
 const context = {
   global,
   metrics,
+  config,
 }
 
 app.get(
--- runtime/programs/main.js
+++ runtime/programs/main.js
@@ -1,6 +1,5 @@
 module.exports = function ({ global }) {
   global.count = global.count + 1
-  global.motor1.startCommand = true
   // console.log(global.motor1)
   // console.log(`Main task has completed ${global.count} times.`)
 }
