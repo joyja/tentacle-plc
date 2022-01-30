66eb710943225014ae68cb5abad303a529621eb8
James Joy <joyja@jarautomation.io>
2022-01-30 18:34:20 +0000

    removed runtime from the repo

--- back/runtime/programs/secondary.js
+++
@@ -1,8 +0,0 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
-
-module.exports = async function ({ global }) {
-  await sleep(100)
-  global.countSecondary = global.countSecondary + 1
-}
